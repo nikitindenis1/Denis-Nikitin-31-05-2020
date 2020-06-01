5c9d3aae5fc023ff2f916540d0f8b4d8d18ca711
denis6421 <denis.b651@gmail.com>
2020-06-01 17:32:04 +0300

    a

--- src/tools/keys.js
+++ src/tools/keys.js
@@ -1,4 +1,4 @@
-export const API_KEY = 'mddA3cL1EE9Seiu5rksrNv8LwYw9L0XE'
+export const API_KEY = 'XgA0S2fmamzvTgdtiFnGodRQYrY3VkR4'
 export const DEFAULT_DESTINATION = 'Tel Aviv'
 export const API = 'http://dataservice.accuweather.com'
 export const IMAGES_API = 'https://developer.accuweather.com/sites/default/files'
